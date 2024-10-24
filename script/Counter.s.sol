4098898eb159cddd9d87c5b06253abc3b549076a
JAA <javi.ares@gmail.com>
2024-10-24 20:39:42 +0200

    build funcionando

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
+import {ERC20} from "../src/ERC20.sol";
 
 contract CounterScript is Script {
     Counter public counter;
@@ -17,3 +18,13 @@ contract CounterScript is Script {
         vm.stopBroadcast();
     }
 }
+
+contract ERC20Script is Script {
+    function run() external {
+        uint256 deployerPrivateKey = INSERT_PRIVATE_KEY;
+        vm.startBroadcast(deployerPrivateKey);
+
+        ERC20 erc20 = new ERC20(1000000000);
+
+        vm.stopBroadcast();
+    }
---
+++ script/ERC20.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import "../src/ERC20.sol";
+// import "../src/MintableERC20.sol";  // Asegúrate de que la ruta sea correcta.
+// import "../src/Rewards.sol";        // Asegúrate de que la ruta sea correcta.
+// import "../src/Factory.sol";        // Asegúrate de que la ruta sea correcta.
+
+contract DeployContractsScript is Script {
+
+    function run() external {
+        // Leer la clave privada desde el archivo .env
+        // uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY_7959");
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        // Iniciar la transacción utilizando la clave privada
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Desplegar la fábrica (Factory) y verifica si los contratos están siendo creados correctamente
+        Factory factory = new Factory();
+        require(address(factory) != address(0), "Factory deployment failed");
+
+        // Desplegar un token utilizando la función `deployToken` de la fábrica
+        string memory tokenName = "Token Name";
+        string memory tokenSymbol = "TKN";
+        uint256 initialSupply = 1000000 * 10 ** 18; // 1 millón de tokens con 18 decimales
+        
+        address tokenAddress = factory.deployToken(tokenName, tokenSymbol, initialSupply);
+        
+        // Obtener la dirección del contrato de recompensas asociado al token
+        address rewardsContractAddress = factory.rewards(factory.rewardsCount() - 1);
+
+        // Mostrar las direcciones desplegadas en la consola para verificar
+        console.log("Token contract deployed at:", tokenAddress);
+        console.log("Rewards contract deployed at:", rewardsContractAddress);
+
+        // Finalizar la transmisión de la transacción
+        vm.stopBroadcast();
+    }
+}
